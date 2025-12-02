1ab32ecef83a8d37717c8449fbcb96b5cd5c687e
Tobias Bordenca <tobiasuzt@gmail.com>
2025-12-02 17:52:44 +0100

    Implement Day 01

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,29 +1,73 @@
 use crate::Day;
 
 pub struct Day01 {
-    nums: Vec<i64>,
+    moves: Vec<i16>,
+}
+impl Day01 {
+    fn parse_move(token: &str) -> i16 {
+        let mut it = token.chars();
+        let dir = it.next().expect("empty move token");
+        let sign = match dir {
+            'L' => -1,
+            'R' => 1,
+            _ => panic!("unexpected direction: {dir}"),
+        };
+        let number: i16 = it.as_str().parse().expect("invalid number");
+        sign * number
+    }
 }
 
 impl Day for Day01 {
     const DAY: u8 = 1;
 
     fn parse(input: &str) -> Self {
-        let nums = input
-            .lines()
-            .filter(|l| !l.trim().is_empty())
-            .map(|l| l.trim().parse::<i64>().expect("expected integer in input"))
-            .collect();
+        let moves = input.split_whitespace().map(Self::parse_move).collect();
 
-        Day01 { nums }
+        Day01 { moves }
     }
 
     fn part1(&self) -> String {
-        let sum: i64 = self.nums.iter().sum();
-        sum.to_string()
+        const START: i16 = 50;
+        const MOD: i16 = 100;
+
+        let mut position = START;
+        let mut count = 0;
+
+        for delta in &self.moves {
+            position = (position + delta).rem_euclid(MOD);
+
+            if position == 0 {
+                count += 1;
+            }
+        }
+
+        count.to_string()
     }
 
     fn part2(&self) -> String {
-        let count = self.nums.iter().filter(|&&x| x > 0).count();
+        const START: i16 = 50;
+        const MOD: i16 = 100;
+
+        let mut position = START;
+        let mut count = 0;
+
+        for delta in &self.moves {
+            count += (delta / MOD).abs(); // Full rotations
+            let start = position;
+            let remaining = delta % MOD;
+
+            position += remaining;
+
+            // No extra clicks if we are at 0
+            if start != 0 {
+                if position <= 0 || position >= MOD {
+                    count += 1;
+                }
+            }
+
+            position = position.rem_euclid(MOD);
+        }
+
         count.to_string()
     }
 }
@@ -54,6 +98,6 @@ L82
     #[test]
     fn part2_example() {
         let d = Day01::parse(EXAMPLE_INPUT);
-        assert_eq!(d.part2(), "3");
+        assert_eq!(d.part2(), "6");
     }
 }
